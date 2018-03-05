7c04b9c53e7741f883a7330d69dfc29ce137809b
eddy <d36584@gmail.com>
2018-03-05 12:12:53 +0800

    first version

---
+++ resources/assets/framework7/js/my-app.js
@@ -0,0 +1,72 @@
+var myApp = '';
+(function() {
+    "use strict";
+    // Initialize your app
+    myApp = new Framework7({
+        fastClicks: true,
+        // modalTitle: '祈机',
+        pushState: true,
+        // swipeBackPage: false,
+        // cache: false,
+        init: false,
+        //debug: true,
+        smartSelectOpenIn: 'picker',
+        animateNavBackIcon: true
+    });
+
+    // Export selectors engine
+    var $$ = Dom7;
+
+    // Add view
+    var mainView = myApp.addView('.view-main', {
+        // Because we use fixed-through navbar we can enable dynamic navbar
+        dynamicNavbar: true
+    });
+
+    myApp.onPageInit('index', function(page) {
+        console.log('index');
+    });
+
+    // Callbacks to run specific code for specific pages, for example for About page:
+    myApp.onPageInit('about', function(page) {
+        console.log('about');
+        // run createContentPage func after link was clicked
+        $$('.create-page').on('click', function() {
+            createContentPage();
+        });
+    });
+    myApp.onPageInit('dynamic-pages', function(page) {
+        console.log('dynamic-pages');
+    });
+
+    // Generate dynamic page
+    var dynamicPageIndex = 0;
+
+    function createContentPage() {
+        mainView.router.loadContent(
+            '<!-- Top Navbar-->' +
+            '<div class="navbar">' +
+            '  <div class="navbar-inner">' +
+            '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
+            '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
+            '  </div>' +
+            '</div>' +
+            '<div class="pages">' +
+            '  <!-- Page, data-page contains page name-->' +
+            '  <div data-page="dynamic-pages" class="page">' +
+            '    <!-- Scrollable page content-->' +
+            '    <div class="page-content">' +
+            '      <div class="content-block">' +
+            '        <div class="content-block-inner">' +
+            '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
+            '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
+            '        </div>' +
+            '      </div>' +
+            '    </div>' +
+            '  </div>' +
+            '</div>'
+        );
+        return;
+    }
+    myApp.init();
+})();
